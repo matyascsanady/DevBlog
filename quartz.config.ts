7ff5e0835aaed5d38e0ec0ca4b403ebf0d92883a
Mátyás Csanády <matyas.csanady@gmail.com>
2025-12-15 14:08:45 +0100

    Quartz sync: Dec 15, 2025, 2:08 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,8 +8,8 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
-    pageTitleSuffix: "",
+    pageTitle: "Dev Blog",
+    pageTitleSuffix: " - Mátyás Csanády",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
